c76563e81af567e6c751c16ea39894420029bb2b
Jaiaid <jaiaidmobin@gmail.com>
2022-04-28 02:33:40 +0600

    public method for passenger draining and filling from or to node and event log

--- networkprimitive.py
+++ networkprimitive.py
@@ -1,5 +1,7 @@
 import simpy
 
+from logger import Logger
+
 
 class Edge(simpy.Resource):
     def __len__(self):
@@ -9,6 +11,7 @@ class Edge(simpy.Resource):
         self.length = length
         self.src_id = src_id
         self.dst_id = dst_id
+        self.env = env
         super().__init__(env, capacity)
 
 
@@ -16,7 +19,22 @@ class Node(simpy.Resource):
     def __init__(self, node_id: int, env: simpy.Environment, capacity: int, dest_id_passenger_dict: dict[int, int]):
         self.id = node_id
         self.dest_id_passenger_dict = dest_id_passenger_dict
+        self.env = env
         super().__init__(env, capacity)
+
+    def drain(self, route_id: int, vehicle_id: int, dest_id: int, count: int) -> int:
+        if dest_id in self.dest_id_passenger_dict:
+            boarding = min(count, self.dest_id_passenger_dict[dest_id])
+            Logger.log(
+                "route {0} vehicle {1} boarding {2} passenger for {3} from {4} at {5}".format(
+                    route_id, vehicle_id, boarding, dest_id, self.id, self.env.now)
+            )
+            self.dest_id_passenger_dict[dest_id] -= boarding
+
+            return 0
+
+    def fill(self, dest_id_passenger_inc_dict: dict[int, int]):
+        pass
 
 
 class Route:

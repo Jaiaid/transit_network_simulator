5acfbda90c9d047289455c4d5f62341a9860c10a
Jaiaid <jaiaidmobin@gmail.com>
2022-04-28 23:07:08 +0600

    average velocity graph drawing with constant resolution is implemented

--- graph_generator.py
+++ graph_generator.py
@@ -1,10 +1,22 @@
 import re
 import matplotlib.pyplot as plot
+import copy
+
+from network import Network
 
 DATA_FILE_NAME = "event_log.txt"
 
 REGEX_VEHICLE_EVENT_LINE_TYPE1 = r"^route (\d+) vehicle (\d+) ([a-z_]+) (\d+) at (\d+\.?\d*)"
 REGEX_VEHICLE_EVENT_LINE_TYPE2 = r"^route (\d+) vehicle (\d+) ([a-z_]+) at (\d+\.?\d*)"
+REGEX_VEHICLE_EVENT_LINE_TYPE3 = \
+    r"^route (\d+) vehicle (\d+) ([a-z_]+) edge (\d+),(\d+) of length (\d+) at (\d+\.?\d*)"
+REGEX_VEHICLE_EVENT_LINE_TYPE4 = \
+    r"^route (\d+) vehicle (\d+) ([a-z_]+) (\d+) passenger for (\d+) from (\d+) at (\d+\.?\d*)"
+REGEX_VEHICLE_EVENT_LINE_TYPE5 = r"^route (\d+) vehicle (\d+) ([a-z_]+) (\d+) passenger for (\d+) at (\d+\.?\d*)"
+REGEX_VEHICLE_EVENT_LINE_TYPE6 = r"^total vehicle (\d+) at (\d+\.?\d*)"
+
+VELOCITY_TIME_RESOLUTION_SEC = 1
+
 REGEX_WAITTIME = r"^Waiting time: (\d*\.\d+)"
 REGEX_EVACTIME = r"^Total Evacuation Time: (\d*\.\d+)"
 REGEX_TRIPCOUNT = r"^Number of trips: (\d+)"
@@ -12,11 +24,87 @@ REGEX_EVACUEECOUNT = r"^Number of evacuaees: (\d+)"
 REGEX_RESULT_FILENAME = r"result(\d+).txt"
 
 
+class SpeedBin:
+    def __init__(self):
+        self.total_travel_length = 0
+        self.running_vehicle_id_dict = {}
+
+    def add_travel_length(self, vehicle_id: int, length: float):
+        self.total_travel_length += length
+        if vehicle_id not in self.running_vehicle_id_dict:
+            self.running_vehicle_id_dict[vehicle_id] = 0
+        self.running_vehicle_id_dict[vehicle_id] += 1
+
+    def get_mean(self) -> float:
+        if len(self.running_vehicle_id_dict.keys()) > 0:
+            return self.total_travel_length / len(self.running_vehicle_id_dict.keys())
+        return 0
+
+
+class SpeedBinContainer:
+    def __init__(self, resolution):
+        self.resolution = resolution
+        self.speed_bin_dict: dict[int, SpeedBin] = {}
+        self.vehicle_latest_entry_dict: dict[int, int] = {}
+
+    def vehicle_enter_data_entry(self, vehicle_id: int, entry_time: int):
+        self.vehicle_latest_entry_dict[vehicle_id] = entry_time
+
+    def vehicle_leave_data_entry(self, vehicle_id: int, length: int, leave_time: int):
+        entry_time = self.vehicle_latest_entry_dict[vehicle_id]
+        entry_event_bin = (entry_time//self.resolution) * self.resolution
+        leave_event_bin = (leave_time//self.resolution) * self.resolution
+
+        if entry_event_bin == leave_event_bin:
+            if entry_event_bin not in self.speed_bin_dict:
+                self.speed_bin_dict[entry_event_bin] = SpeedBin()
+            self.speed_bin_dict[entry_event_bin].add_travel_length(vehicle_id=vehicle_id, length=length)
+        else:
+            for event_bin in range(entry_event_bin, leave_event_bin, self.resolution):
+                if event_bin not in self.speed_bin_dict:
+                    self.speed_bin_dict[entry_event_bin] = SpeedBin()
+
+            if leave_event_bin not in self.speed_bin_dict:
+                self.speed_bin_dict[leave_event_bin] = SpeedBin()
+
+            prev_time = entry_time
+            prev_event_bin = entry_event_bin
+            for event_bin in range(entry_event_bin + self.resolution, leave_event_bin,
+                                   self.resolution):
+                portion = ((event_bin - prev_time) / (leave_time - entry_time)) * length
+
+                if prev_event_bin not in self.speed_bin_dict:
+                    self.speed_bin_dict[prev_event_bin] = SpeedBin()
+
+                self.speed_bin_dict[prev_event_bin].add_travel_length(vehicle_id=vehicle_id, length=portion)
+                prev_event_bin = event_bin
+                prev_time = event_bin
+
+            portion = ((leave_time - prev_event_bin) / (leave_time - entry_time)) * length
+            self.speed_bin_dict[leave_event_bin].add_travel_length(vehicle_id=vehicle_id, length=portion)
+
+    def generate_graph(self):
+        fig_avg_speed, ax_avg_speed = plot.subplots()
+
+        x_coords = sorted(list(self.speed_bin_dict.keys()))
+        y_coords = []
+        for x in x_coords:
+            y_coords.append(self.speed_bin_dict[x].get_mean())
+
+        ax_avg_speed.plot(x_coords, y_coords)
+        ax_avg_speed.set_title("average velocity in {0}s resolution".format(self.resolution))
+        ax_avg_speed.legend()
+        ax_avg_speed.set_xlabel("second")
+        ax_avg_speed.set_ylabel("unit/sec")
+        fig_avg_speed.savefig("average_velocity.png", dpi=300)
+
+
 class GraphGenerator:
     def __init__(self):
         self.hourly_trip_completion_stat: dict[int, int] = {}
         self.hourly_trip_start_stat: dict[int, int] = {}
         self.hourly_speed_stat: dict[int, float] = {}
+        self.speedbin_container = SpeedBinContainer(resolution=VELOCITY_TIME_RESOLUTION_SEC)
 
     def __analyze(self):
         with open(DATA_FILE_NAME) as log_fin:
@@ -26,6 +114,12 @@ class GraphGenerator:
                 result = re.search(REGEX_VEHICLE_EVENT_LINE_TYPE1, logline)
                 if result is None:
                     result = re.search(REGEX_VEHICLE_EVENT_LINE_TYPE2, logline)
+                if result is None:
+                    result = re.search(REGEX_VEHICLE_EVENT_LINE_TYPE3, logline)
+                if result is None:
+                    result = re.search(REGEX_VEHICLE_EVENT_LINE_TYPE4, logline)
+                if result is None:
+                    result = re.search(REGEX_VEHICLE_EVENT_LINE_TYPE5, logline)
                 if result is None:
                     continue
 
@@ -49,6 +143,18 @@ class GraphGenerator:
                         self.hourly_trip_completion_stat[hour] = 1
                     else:
                         self.hourly_trip_completion_stat[hour] += 1
+                elif event_type == "entering":
+                    timestamp = int(result.groups()[6])
+                    self.speedbin_container.vehicle_enter_data_entry(vehicle_id=vehicle_id, entry_time=timestamp)
+                elif event_type == "leaving":
+                    length = int(result.groups()[5])
+                    timestamp = int(result.groups()[6])
+                    self.speedbin_container.vehicle_leave_data_entry(vehicle_id=vehicle_id, length=length,
+                                                                     leave_time=timestamp)
+                elif event_type == "boarding":
+                    pass
+                elif event_type == "offloading":
+                    pass
         # print(self.hourly_trip_start_stat)
         # print(self.hourly_trip_completion_stat)
 
@@ -75,3 +181,4 @@ class GraphGenerator:
         ax_trip_complete.set_ylabel("count")
         fig_trip_complete.savefig("hourly_trip_completion_barplot.png", dpi=300)
 
+        self.speedbin_container.generate_graph()

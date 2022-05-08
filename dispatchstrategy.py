870e25064b638a0e024a9917412dc6ab661d49c0
Jaiaid <jaiaidmobin@gmail.com>
2022-05-08 17:08:58 +0600

    observe method refactored to update_route method

--- dispatchstrategy.py
+++ dispatchstrategy.py
@@ -2,7 +2,7 @@ import simpy
 
 from dispatcher import Dispatcher
 from network import Network
-
+from vehicle import Vehicle
 
 class DispatchStrategy:
     def __init__(self, dispatcher: Dispatcher, env: simpy.Environment):
@@ -17,6 +17,7 @@ class DispatchStrategy:
             route_id += 1
             route_id %= len(self.dispatcher.network.route_list)
 
-    def observe(self):
+    def update_route(self, network: Network, vehicle: Vehicle) -> bool:
         if any(self.dispatcher.completion_flag):
             pass
+        return True

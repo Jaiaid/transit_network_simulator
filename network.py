4f4747d592369c9d72bec428914aa338bff5c1cb
Jaiaid <jaiaidmobin@gmail.com>
2022-05-26 12:23:31 +0600

    node class load on runtime when loading network data

--- network.py
+++ network.py
@@ -1,4 +1,7 @@
 import simpy
+import os
+import sys
+import importlib.util
 
 from networkprimitive import Edge, Node, Route
 
@@ -13,6 +16,7 @@ class NetworkEdgeData:
         return self.cap_data[src_id][dst_id]
 
     def load_data(self, filepath: str, ):
+        self.cap_data = []
         with open(filepath) as fin:
             for line in fin.readlines():
                 self.cap_data.append([])
@@ -33,10 +37,12 @@ class NetworkNodeData:
         return self.demand_dict_list[node_id]
 
     def load_data(self, demand_filepath: str, vehicle_capfilepath: str):
+        self.cap_data = []
         with open(vehicle_capfilepath) as fin:
             for line in fin.readlines():
                 self.cap_data.append(int(line))
 
+        self.demand_dict_list = []
         with open(demand_filepath) as fin:
             src_id = 0
             for line in fin.readlines():
@@ -56,6 +62,20 @@ class Network:
         self.route_list: list[Route] = []
         self.edge_dict: dict[(int, int), Edge] = {}
         self.node_list: list[Node] = []
+        self.node_class = Node
+
+    def __load_node_class(self, node_class_full_import_string: str):
+        import_data = node_class_full_import_string.split(".")
+        module_path = ".".join(import_data[:-1])
+        # extract module name by removing .py from basepath
+        module_name = ".".join(os.path.basename(module_path).split(".")[:-1])
+        class_name = import_data[-1]
+
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        self.node_class = getattr(module, class_name)
 
     def get_route(self, route_id: int):
         return self.route_list[route_id]
@@ -80,17 +100,20 @@ class Network:
                 route_id += 1
 
     def load_network_data(self, network_filepath: str, network_edgecap_filepath: str, network_demand_filepath: str,
-                          network_nodecap_filepath: str):
+                          network_nodecap_filepath: str, node_class_script_path: str):
+        self.__load_node_class(node_class_full_import_string=node_class_script_path + ".Node")
         self.edge_cap_data.load_data(network_edgecap_filepath)
         self.node_data.load_data(network_demand_filepath, network_nodecap_filepath)
 
+        self.node_list = []
         with open(network_filepath) as fin:
             src_id = 0
             for line in fin.readlines():
                 dst_id = 0
 
-                self.node_list.append(Node(node_id=src_id, env=self.env, capacity=self.node_data.get_cap(src_id),
-                                           dest_id_passenger_dict=self.node_data.get_demand_dict(src_id)))
+                self.node_list.append(self.node_class(node_id=src_id, env=self.env,
+                                                      capacity=self.node_data.get_cap(src_id),
+                                                      dest_id_passenger_dict=self.node_data.get_demand_dict(src_id)))
 
                 for token in line.split():
                     if float(token) != INF_CAP:

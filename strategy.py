e99ff904301964f4c16ec54b412a22857146a3ba
Jaiaid <jaiaidmobin@gmail.com>
2022-05-26 12:18:44 +0600

    strategy related classes are in same script

--- strategy.py
+++ strategy.py
@@ -2,6 +2,7 @@ import simpy
 import copy
 
 from networkprimitive import Node, Edge, Route
+from network import Network
 from vehicle import Vehicle
 from dispatcher import Dispatcher
 
@@ -70,3 +71,22 @@ class VehicleStrategy:
 
     def signal_completion(self):
         self.dispatcher.notify(self.vehicle.id)
+
+
+class DispatchStrategy:
+    def __init__(self, dispatcher: Dispatcher, env: simpy.Environment):
+        self.dispatcher: Dispatcher = dispatcher
+        self.env = env
+
+    def assign_route(self, network: Network):
+        # basic round robin assignment
+        route_id = 0
+        for vehicle_id in self.dispatcher.fleet.vehicle_dict:
+            self.dispatcher.fleet.vehicle_dict[vehicle_id].route_id = self.dispatcher.network.route_list[route_id].id
+            route_id += 1
+            route_id %= len(self.dispatcher.network.route_list)
+
+    def update_route(self, network: Network, vehicle: Vehicle) -> bool:
+        if any(self.dispatcher.completion_flag):
+            pass
+        return True
